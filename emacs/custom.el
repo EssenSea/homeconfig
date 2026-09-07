f24d27c1c546a75243f8241f569cca59aaa58a6e
EssenMoon <yueqrgg@gmail.com>
2026-09-07 14:27:29 +0800

    chore: 提交 custom.el 并扩展 .gitignore 忽略运行时产物
    - custom.el: 同步 package-selected-packages 列表
    - .gitignore: 新增 elpa/ eln-cache/ emms/ var/ recentf.eld
      package-quickstart.el 等运行时生成的内容

--- emacs/custom.el
+++ emacs/custom.el
@@ -6,16 +6,16 @@
  ;; If there is more than one, they won't work right.
  '(large-file-warning-threshold (expt 2 22))
  '(package-selected-packages
-   '(activities aidermacs auctex avy cape cdlatex circadian corfu daemons
+   '(activities aidermacs auctex avy cape cdlatex circadian consult corfu daemons
                 dashboard disk-usage doom-modeline eat elfeed ellama elpher
-                embark-consult emms empv eselect-news gptel helpful
-                kaolin-themes kind-icon logview magit marginalia minuet
+                embark embark-consult emms empv eselect-news gptel helpful
+                kaolin-themes kind-icon llm logview magit marginalia minuet
                 nerd-icons-completion nerd-icons-dired nerd-icons-ibuffer nov
                 olivetti orderless org-roam org-superstar ox-typst perspective
                 pinentry portage-modes prescient rainbow-delimiters
-                switchy-window tempel-collection transient-posframe
-                treesit-auto typst-ts-mode undo-fu undo-fu-session
-                vertico-posframe vlf vundo wgrep which-key-posframe
+                switchy-window tempel tempel-collection transient-posframe
+                treesit-auto typst-ts-mode undo-fu undo-fu-session vertico
+                vertico-posframe vlf vundo wgrep which-key-posframe yasnippet
                 yasnippet-capf yasnippet-snippets))
  '(vlf-application 'dont-ask))
 (custom-set-faces
