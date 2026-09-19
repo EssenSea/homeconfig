392a1de876f916dd22cbf371ca784612ae5e130c
EssenMoon <yueqrgg@gmail.com>
2026-09-20 01:00:54 +0800

    replaced telescope and relatives with snack.picker

--- nvim/init.lua
+++ nvim/init.lua
@@ -364,13 +364,13 @@ do
       local kind = ev.data.kind
       if kind ~= 'install' and kind ~= 'update' then return end
 
-      if
-        name == 'telescope-fzf-native.nvim'
-        and vim.fn.executable 'make' == 1
-      then
-        run_build(name, { 'make' }, ev.data.path)
-        return
-      end
+      -- if
+      --   name == 'telescope-fzf-native.nvim'
+      --   and vim.fn.executable 'make' == 1
+      -- then
+      --   run_build(name, { 'make' }, ev.data.path)
+      --   return
+      -- end
 
       if name == 'LuaSnip' then
         if vim.fn.has 'win32' ~= 1 and vim.fn.executable 'make' == 1 then
@@ -663,7 +663,7 @@ end
 -- SECTION 5: SEARCH & NAVIGATION
 -- Telescope setup, keymaps, LSP picker mappings
 -- ============================================================
-do
+-- do
   -- [[ Fuzzy Finder (files, lsp, etc) ]]
   --
   -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -689,221 +689,277 @@ do
   -- do as well as how to actually do it!
 
   ---@type (string|vim.pack.Spec)[]
-  local telescope_plugins = {
-    gh 'nvim-lua/plenary.nvim',
-    gh 'nvim-telescope/telescope.nvim',
-    gh 'nvim-telescope/telescope-ui-select.nvim',
-  }
-  if vim.fn.executable 'make' == 1 then
-    table.insert(
-      telescope_plugins,
-      gh 'nvim-telescope/telescope-fzf-native.nvim'
-    )
-  end
-
-  -- NOTE: You can install multiple plugins at once
-  vim.pack.add(telescope_plugins)
-
-  -- See `:help telescope` and `:help telescope.setup()`
-  require('telescope').setup {
-    -- You can put your default mappings / updates / etc. in here
-    --  All the info you're looking for is in `:help telescope.setup()`
-    --
-    defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      layout_strategy = 'bottom_pane',
-      layout_config = {
-        height = 0.99,
-      },
-    },
-    -- pickers = {}
-    themes = function()
-      local builtin = require 'telescope.builtin'
-      local themes = require 'telescope.themes'
-      builtin.find_files(themes.get_ivy())
-    end,
-    extensions = {
-      ['ui-select'] = { require('telescope.themes').get_dropdown() },
-    },
-  }
-
-  -- Enable Telescope extensions if they are installed
-  pcall(require('telescope').load_extension, 'fzf')
-  pcall(require('telescope').load_extension, 'ui-select')
-
-  -- See `:help telescope.builtin`
-  local builtin = require 'telescope.builtin'
-  vim.keymap.set(
-    'n',
-    '<leader>sh',
-    builtin.help_tags,
-    { desc = '[S]earch [H]elp' }
-  )
-  vim.keymap.set(
-    'n',
-    '<leader>sk',
-    builtin.keymaps,
-    { desc = '[S]earch [K]eymaps' }
-  )
-  vim.keymap.set(
-    'n',
-    '<leader>sf',
-    builtin.find_files,
-    { desc = '[S]earch [F]iles' }
-  )
-  vim.keymap.set(
-    'n',
-    '<leader>ss',
-    builtin.builtin,
-    { desc = '[S]earch [S]elect Telescope' }
-  )
-  vim.keymap.set(
-    { 'n', 'v' },
-    '<leader>sw',
-    builtin.grep_string,
-    { desc = '[S]earch current [W]ord' }
-  )
-  vim.keymap.set(
-    'n',
-    '<leader>sg',
-    builtin.live_grep,
-    { desc = '[S]earch by [G]rep' }
-  )
-  vim.keymap.set(
-    'n',
-    '<leader>sd',
-    builtin.diagnostics,
-    { desc = '[S]earch [D]iagnostics' }
-  )
-  vim.keymap.set(
-    'n',
-    '<leader>sr',
-    builtin.resume,
-    { desc = '[S]earch [R]esume' }
-  )
-  vim.keymap.set(
-    'n',
-    '<leader>s.',
-    builtin.oldfiles,
-    { desc = '[S]earch Recent Files ("." for repeat)' }
-  )
-  vim.keymap.set(
-    'n',
-    '<leader>sc',
-    builtin.commands,
-    { desc = '[S]earch [C]ommands' }
-  )
-  vim.keymap.set(
-    'n',
-    '<leader><leader>',
-    builtin.buffers,
-    { desc = '[ ] Find existing buffers' }
-  )
-
-  -- Add Telescope-based LSP pickers when an LSP attaches to a buffer.
-  -- If you later switch picker plugins, this is where to update these mappings.
-  vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup(
-      'telescope-lsp-attach',
-      { clear = true }
-    ),
-    callback = function(event)
-      local buf = event.buf
-
-      -- Find references for the word under your cursor.
-      vim.keymap.set(
-        'n',
-        'grr',
-        builtin.lsp_references,
-        { buffer = buf, desc = '[G]oto [R]eferences' }
-      )
-
-      -- Jump to the implementation of the word under your cursor.
-      -- Useful when your language has ways of declaring types without an actual implementation.
-      vim.keymap.set(
-        'n',
-        'gri',
-        builtin.lsp_implementations,
-        { buffer = buf, desc = '[G]oto [I]mplementation' }
-      )
-
-      -- Jump to the definition of the word under your cursor.
-      -- This is where a variable was first declared, or where a function is defined, etc.
-      -- To jump back, press <C-t>.
-      vim.keymap.set(
-        'n',
-        'grd',
-        builtin.lsp_definitions,
-        { buffer = buf, desc = '[G]oto [D]efinition' }
-      )
-
-      -- Fuzzy find all the symbols in your current document.
-      -- Symbols are things like variables, functions, types, etc.
-      vim.keymap.set(
-        'n',
-        'gO',
-        builtin.lsp_document_symbols,
-        { buffer = buf, desc = 'Open Document Symbols' }
-      )
-
-      -- Fuzzy find all the symbols in your current workspace.
-      -- Similar to document symbols, except searches over your entire project.
-      vim.keymap.set(
-        'n',
-        'gW',
-        builtin.lsp_dynamic_workspace_symbols,
-        { buffer = buf, desc = 'Open Workspace Symbols' }
-      )
-
-      -- Jump to the type of the word under your cursor.
-      -- Useful when you're not sure what type a variable is and you want to see
-      -- the definition of its *type*, not where it was *defined*.
-      vim.keymap.set(
-        'n',
-        'grt',
-        builtin.lsp_type_definitions,
-        { buffer = buf, desc = '[G]oto [T]ype Definition' }
-      )
-    end,
-  })
-
-  -- Override default behavior and theme when searching
-  vim.keymap.set('n', '<leader>/', function()
-    -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-    builtin.current_buffer_fuzzy_find(
-      require('telescope.themes').get_dropdown {
-        winblend = 0,
-        previewer = false,
-      }
-    )
-  end, { desc = '[/] Fuzzily search in current buffer' })
-
-  -- It's also possible to pass additional configuration options.
-  --  See `:help telescope.builtin.live_grep()` for information about particular keys
-  vim.keymap.set(
-    'n',
-    '<leader>s/',
-    function()
-      builtin.live_grep {
-        grep_open_files = true,
-        prompt_title = 'Live Grep in Open Files',
-      }
-    end,
-    { desc = '[S]earch [/] in Open Files' }
-  )
-
-  -- Shortcut for searching your Neovim configuration files
-  vim.keymap.set(
-    'n',
-    '<leader>sn',
-    function()
-      builtin.find_files { cwd = vim.fn.stdpath 'config', follow = true }
-    end,
-    { desc = '[S]earch [N]eovim files' }
-  )
-end
+--   local telescope_plugins = {
+--     gh 'nvim-lua/plenary.nvim',
+--     gh 'nvim-telescope/telescope.nvim',
+--     gh 'nvim-telescope/telescope-ui-select.nvim',
+--   }
+--   if vim.fn.executable 'make' == 1 then
+--     table.insert(
+--       telescope_plugins,
+--       gh 'nvim-telescope/telescope-fzf-native.nvim'
+--     )
+--   end
+--
+--   -- NOTE: You can install multiple plugins at once
+--   vim.pack.add(telescope_plugins)
+--
+--   -- See `:help telescope` and `:help telescope.setup()`
+--   require('telescope').setup {
+--     -- You can put your default mappings / updates / etc. in here
+--     --  All the info you're looking for is in `:help telescope.setup()`
+--     --
+--     defaults = {
+--       --   mappings = {
+--       --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+--       --   },
+--       dynamic_preview_title = true,
+--       files_ignore_pattern = {
+--         '%.git',
+--         '^node_modules/',
+--         '^%.cache/',
+--         '^%.cargo/',
+--         '^%.asy',
+--         '^%.bun',
+--         '^%.cmake/',
+--         '^%.dbus/',
+--         '^%.dsh',
+--         '^%.mozilla/',
+--         '^%.mypy_cache',
+--         '^pyvenv/',
+--         '^%.npm/',
+--         '%.npm',
+--         '%.npz',
+--         '^eln-cache/',
+--         '%.elc',
+--         '%.log',
+--         '^log/',
+--         '^logs/',
+--         '^%.wine/',
+--         '^%.steam/',
+--         '^%.thunderbird/',
+--         '^db/',
+--         '^Videos/',
+--         '^Pictures/',
+--       },
+--       layout_strategy = 'flex',
+--       layout_config = {
+--         height = 0.85,
+--         width = 0.85,
+--         horizontal = {
+--           preview_width = 80,
+--         },
+--         vertical = {
+--           preview_cutoff = 20,
+--           preview_height = 15,
+--         },
+--       },
+--     },
+--     pickers = {
+--       find_files = {
+--         -- follow = true,
+--         hidden = true,
+--       },
+--     },
+--     themes = function()
+--       local builtin = require 'telescope.builtin'
+--       local themes = require 'telescope.themes'
+--       builtin.find_files(themes.get_cursor())
+--     end,
+--     extensions = {
+--       ['ui-select'] = { require('telescope.themes').get_dropdown() },
+--       fzf = {
+--         fuzzy = true, -- false will only do exact matching
+--         override_generic_sorter = true, -- override the generic sorter
+--         override_file_sorter = true, -- override the file sorter
+--         case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
+--         -- the default case_mode is "smart_case"
+--       },
+--     },
+--   }
+--
+--   -- Enable Telescope extensions if they are installed
+--   pcall(require('telescope').load_extension, 'fzf')
+--   pcall(require('telescope').load_extension, 'ui-select')
+--
+--   -- See `:help telescope.builtin`
+--   local builtin = require 'telescope.builtin'
+--   vim.keymap.set(
+--     'n',
+--     '<leader>sh',
+--     builtin.help_tags,
+--     { desc = '[S]earch [H]elp' }
+--   )
+--   vim.keymap.set(
+--     'n',
+--     '<leader>sk',
+--     builtin.keymaps,
+--     { desc = '[S]earch [K]eymaps' }
+--   )
+--   vim.keymap.set(
+--     'n',
+--     '<leader>sf',
+--     builtin.find_files,
+--     { desc = '[S]earch [F]iles' }
+--   )
+--   vim.keymap.set(
+--     'n',
+--     '<leader>ss',
+--     builtin.builtin,
+--     { desc = '[S]earch [S]elect Telescope' }
+--   )
+--   vim.keymap.set(
+--     { 'n', 'v' },
+--     '<leader>sw',
+--     builtin.grep_string,
+--     { desc = '[S]earch current [W]ord' }
+--   )
+--   vim.keymap.set(
+--     'n',
+--     '<leader>sg',
+--     builtin.live_grep,
+--     { desc = '[S]earch by [G]rep' }
+--   )
+--   vim.keymap.set(
+--     'n',
+--     '<leader>sd',
+--     builtin.diagnostics,
+--     { desc = '[S]earch [D]iagnostics' }
+--   )
+--   vim.keymap.set(
+--     'n',
+--     '<leader>sr',
+--     builtin.resume,
+--     { desc = '[S]earch [R]esume' }
+--   )
+--   vim.keymap.set(
+--     'n',
+--     '<leader>s.',
+--     builtin.oldfiles,
+--     { desc = '[S]earch Recent Files ("." for repeat)' }
+--   )
+--   vim.keymap.set(
+--     'n',
+--     '<leader>sc',
+--     builtin.commands,
+--     { desc = '[S]earch [C]ommands' }
+--   )
+--   vim.keymap.set(
+--     'n',
+--     '<leader><leader>',
+--     builtin.buffers,
+--     { desc = '[ ] Find existing buffers' }
+--   )
+--
+--   -- Add Telescope-based LSP pickers when an LSP attaches to a buffer.
+--   -- If you later switch picker plugins, this is where to update these mappings.
+--   vim.api.nvim_create_autocmd('LspAttach', {
+--     group = vim.api.nvim_create_augroup(
+--       'telescope-lsp-attach',
+--       { clear = true }
+--     ),
+--     callback = function(event)
+--       local buf = event.buf
+--
+--       -- Find references for the word under your cursor.
+--       vim.keymap.set(
+--         'n',
+--         'grr',
+--         builtin.lsp_references,
+--         { buffer = buf, desc = '[G]oto [R]eferences' }
+--       )
+--
+--       -- Jump to the implementation of the word under your cursor.
+--       -- Useful when your language has ways of declaring types without an actual implementation.
+--       vim.keymap.set(
+--         'n',
+--         'gri',
+--         builtin.lsp_implementations,
+--         { buffer = buf, desc = '[G]oto [I]mplementation' }
+--       )
+--
+--       -- Jump to the definition of the word under your cursor.
+--       -- This is where a variable was first declared, or where a function is defined, etc.
+--       -- To jump back, press <C-t>.
+--       vim.keymap.set(
+--         'n',
+--         'grd',
+--         builtin.lsp_definitions,
+--         { buffer = buf, desc = '[G]oto [D]efinition' }
+--       )
+--
+--       -- Fuzzy find all the symbols in your current document.
+--       -- Symbols are things like variables, functions, types, etc.
+--       vim.keymap.set(
+--         'n',
+--         'gO',
+--         builtin.lsp_document_symbols,
+--         { buffer = buf, desc = 'Open Document Symbols' }
+--       )
+--
+--       -- Fuzzy find all the symbols in your current workspace.
+--       -- Similar to document symbols, except searches over your entire project.
+--       vim.keymap.set(
+--         'n',
+--         'gW',
+--         builtin.lsp_dynamic_workspace_symbols,
+--         { buffer = buf, desc = 'Open Workspace Symbols' }
+--       )
+--
+--       -- Jump to the type of the word under your cursor.
+--       -- Useful when you're not sure what type a variable is and you want to see
+--       -- the definition of its *type*, not where it was *defined*.
+--       vim.keymap.set(
+--         'n',
+--         'grt',
+--         builtin.lsp_type_definitions,
+--         { buffer = buf, desc = '[G]oto [T]ype Definition' }
+--       )
+--     end,
+--   })
+--
+--   -- Override default behavior and theme when searching
+--   vim.keymap.set('n', '<leader>/', function()
+--     -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+--     builtin.current_buffer_fuzzy_find(
+--       require('telescope.themes').get_dropdown {
+--         winblend = 0,
+--         previewer = false,
+--       }
+--     )
+--   end, { desc = '[/] Fuzzily search in current buffer' })
+--
+--   -- It's also possible to pass additional configuration options.
+--   --  See `:help telescope.builtin.live_grep()` for information about particular keys
+--   vim.keymap.set(
+--     'n',
+--     '<leader>s/',
+--     function()
+--       builtin.live_grep {
+--         grep_open_files = true,
+--         prompt_title = 'Live Grep in Open Files',
+--       }
+--     end,
+--     { desc = '[S]earch [/] in Open Files' }
+--   )
+--
+--   -- Shortcut for searching your Neovim configuration files
+--   vim.keymap.set(
+--     'n',
+--     '<leader>sn',
+--     function()
+--       builtin.find_files { cwd = vim.fn.stdpath 'config', follow = true }
+--     end,
+--     { desc = '[S]earch [N]eovim files' }
+--   )
+--
+--   vim.keymap.set(
+--     'n',
+--     '<leader>th',
+--     function() builtin.colorscheme { enable_preview = true } end,
+--     { desc = 'colorschemes' }
+--   )
+-- end
 
 -- ============================================================
 -- SECTION 6: LSP
--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -64,50 +64,17 @@ require('mini.files').setup {
     go_in_plus = 'l',
   },
 }
-vim.keymap.set({ 'n', 'v' }, '<leader>ff', function()
+vim.keymap.set({ 'n', 'v' }, '<leader>mf', function()
   if MiniFiles.close() == nil then
     MiniFiles.open(vim.api.nvim_buf_get_name(0))
   end
 end, { desc = 'MiniFiles' })
 
-vim.pack.add { gh 'shellRaining/hlchunk.nvim' }
-require('hlchunk').setup {
-  chunk = {
-    enable = true,
-    use_treesitter = true,
-    style = {
-      '#d3c6aa',
-    },
-    chars = {
-      horizontal_line = '─',
-      vertical_line = '│',
-      left_top = '╭',
-      left_bottom = '╰',
-      right_arrow = '>',
-    },
-    notify = true,
-    priority = 0,
-    error_sign = true,
-    exclude_filetypes = {},
-    duration = 200,
-    delay = 50,
-  },
-  indent = {
-    enable = true,
-  },
-  line_num = {
-    enable = true,
-  },
-  blank = {
-    enable = true,
-  },
-}
-
-vim.pack.add {
-  -- gh 'MunifTanjim/nui.nvim',
-  -- gh 'rcarriga/nvim-notify',
-  gh 'folke/noice.nvim',
-}
+-- vim.pack.add {
+-- gh 'MunifTanjim/nui.nvim',
+-- gh 'rcarriga/nvim-notify',
+-- gh 'folke/noice.nvim',
+-- }
 --
 -- vim.notify = require 'notify'
 --
@@ -133,6 +100,7 @@ vim.pack.add {
 vim.pack.add { gh 'aserowy/tmux.nvim' }
 require('tmux').setup()
 
+require('mini.starter').setup {}
 vim.pack.add { gh 'folke/snacks.nvim' }
 require('mini.diff').setup {
   view = {
@@ -143,24 +111,53 @@ require('mini.diff').setup {
 require('snacks').setup {
   ---@type snacks.Config
   -- your configuration comes here
-  -- or leave it empty to use the default settings
-  -- refer to the configuration section below
-  bigfile = { enabled = false },
+  bigfile = { enabled = true },
   dashboard = { enabled = false },
   dim = { enabled = true },
-  explorer = { enabled = true },
-  indent = { enabled = true },
-  input = { enabled = false },
-  picker = { enabled = true },
+  explorer = {
+    enabled = true,
+    replace_netrw = true,
+    follow_file = true,
+  },
+  image = {
+    enabled = true,
+  },
+  indent = {
+    enabled = true,
+    chunk = {
+      enabled = true,
+      char = {
+        corner_top = '╭',
+        corner_bottom = '╰',
+      },
+    },
+  },
+  input = { enabled = true },
+  ---@class snacks.picker
+  ---@class snacks.picker.Config
+  ---@class snacks.picker.layout.Config
+  picker = {
+    enabled = true,
+    layout = {
+      reverse = false,
+      fullscreen = true,
+      preset = 'ivy',
+    },
+    sources = {
+      explorer = {
+        layout = { reverse = false, fullscreen = false, preset = 'sidebar' },
+      },
+    },
+  },
   notifier = {
     enabled = true,
     timeout = 9000,
   },
-  quickfile = { enabled = false },
-  scope = { enabled = false },
+  quickfile = { enabled = true },
+  scope = { enabled = true },
   scroll = { enabled = false },
-  statuscolumn = { enabled = false },
-  words = { enabled = false },
+  statuscolumn = { enabled = true },
+  words = { enabled = true },
   styles = {
     zen = {
       keys = { q = 'close' },
@@ -179,7 +176,7 @@ require('snacks').setup {
       mini_diff_signs = true,
     },
     win = {
-      width = 100,
+      width = 90,
       height = 30,
       backdrop = {
         transparent = false,
@@ -188,18 +185,371 @@ require('snacks').setup {
     },
   },
 }
-
+-- Top Pickers & Explorer
 vim.keymap.set(
-  { 'n', 'v' },
-  '<leader>fs',
-  function() Snacks.explorer() end,
-  { desc = 'Snacks.explorer' }
+  { 'n' },
+  '<leader><space>',
+  function() Snacks.picker.smart() end,
+  { desc = 'Smart Find Files' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>,',
+  function() Snacks.picker.buffers() end,
+  { desc = 'Buffers' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>/',
+  function() Snacks.picker.grep() end,
+  { desc = 'Grep' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>:',
+  function() Snacks.picker.command_history() end,
+  { desc = 'Command History' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>n',
+  function() Snacks.picker.notifications() end,
+  { desc = 'Notification History' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>e',
+  function() Snacks.explorer.open { follow_file = true } end,
+  { desc = 'File Explorer' }
+)
+-- find
+vim.keymap.set(
+  { 'n' },
+  '<leader>fb',
+  function() Snacks.picker.buffers() end,
+  { desc = 'Buffers' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>fc',
+  function()
+    Snacks.picker.files { cwd = vim.fn.stdpath 'config', follow = true }
+  end,
+  { desc = 'Find Config File' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>ff',
+  function() Snacks.picker.files() end,
+  { desc = 'Find Files' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>fg',
+  function() Snacks.picker.git_files() end,
+  { desc = 'Find Git Files' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>fp',
+  function() Snacks.picker.projects() end,
+  { desc = 'Projects' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>fr',
+  function() Snacks.picker.recent() end,
+  { desc = 'Recent' }
+)
+-- git
+vim.keymap.set(
+  { 'n' },
+  '<leader>gb',
+  function() Snacks.picker.git_branches() end,
+  { desc = 'Git Branches' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>gl',
+  function() Snacks.picker.git_log() end,
+  { desc = 'Git Log' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>gL',
+  function() Snacks.picker.git_log_line() end,
+  { desc = 'Git Log Line' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>gs',
+  function() Snacks.picker.git_status() end,
+  { desc = 'Git Status' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>gS',
+  function() Snacks.picker.git_stash() end,
+  { desc = 'Git Stash' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>gd',
+  function() Snacks.picker.git_diff() end,
+  { desc = 'Git Diff (Hunks)' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>gf',
+  function() Snacks.picker.git_log_file() end,
+  { desc = 'Git Log File' }
+)
+-- gh
+vim.keymap.set(
+  { 'n' },
+  '<leader>gi',
+  function() Snacks.picker.gh_issue() end,
+  { desc = 'GitHub Issues (open)' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>gI',
+  function() Snacks.picker.gh_issue { state = 'all' } end,
+  { desc = 'GitHub Issues (all)' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>gp',
+  function() Snacks.picker.gh_pr() end,
+  { desc = 'GitHub Pull Requests (open)' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>gP',
+  function() Snacks.picker.gh_pr { state = 'all' } end,
+  { desc = 'GitHub Pull Requests (all)' }
+)
+-- Grep
+vim.keymap.set(
+  { 'n' },
+  '<leader>sb',
+  function() Snacks.picker.lines() end,
+  { desc = 'Buffer Lines' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sB',
+  function() Snacks.picker.grep_buffers() end,
+  { desc = 'Grep Open Buffers' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sg',
+  function() Snacks.picker.grep() end,
+  { desc = 'Grep' }
+)
+vim.keymap.set(
+  { 'n', 'x' },
+  '<leader>sw',
+  function() Snacks.picker.grep_word() end,
+  { desc = 'Visual selection or word' }
+)
+-- search
+vim.keymap.set(
+  { 'n' },
+  '<leader>s"',
+  function() Snacks.picker.registers() end,
+  { desc = 'Registers' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>s/',
+  function() Snacks.picker.search_history() end,
+  { desc = 'Search History' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sa',
+  function() Snacks.picker.autocmds() end,
+  { desc = 'Autocmds' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sb',
+  function() Snacks.picker.lines() end,
+  { desc = 'Buffer Lines' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sc',
+  function() Snacks.picker.command_history() end,
+  { desc = 'Command History' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sC',
+  function() Snacks.picker.commands() end,
+  { desc = 'Commands' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sd',
+  function() Snacks.picker.diagnostics() end,
+  { desc = 'Diagnostics' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sD',
+  function() Snacks.picker.diagnostics_buffer() end,
+  { desc = 'Buffer Diagnostics' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sh',
+  function() Snacks.picker.help() end,
+  { desc = 'Help Pages' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sH',
+  function() Snacks.picker.highlights() end,
+  { desc = 'Highlights' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>si',
+  function() Snacks.picker.icons() end,
+  { desc = 'Icons' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sj',
+  function() Snacks.picker.jumps() end,
+  { desc = 'Jumps' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sk',
+  function() Snacks.picker.keymaps() end,
+  { desc = 'Keymaps' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sl',
+  function() Snacks.picker.loclist() end,
+  { desc = 'Location List' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sm',
+  function() Snacks.picker.marks() end,
+  { desc = 'Marks' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sM',
+  function() Snacks.picker.man() end,
+  { desc = 'Man Pages' }
+)
+-- vim.keymap.set(
+--   { 'n' },
+--   '<leader>sp',
+--   function() Snacks.picker.lazy() end,
+--   { desc = 'Search for Plugin Spec' }
+-- )
+vim.keymap.set(
+  { 'n' },
+  '<leader>sq',
+  function() Snacks.picker.qflist() end,
+  { desc = 'Quickfix List' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sR',
+  function() Snacks.picker.resume() end,
+  { desc = 'Resume' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>su',
+  function() Snacks.picker.undo() end,
+  { desc = 'Undo History' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>uC',
+  function() Snacks.picker.colorschemes() end,
+  { desc = 'Colorschemes' }
+)
+vim.keymap.set(
+  { 'n' },
+  'gd',
+  function() Snacks.picker.lsp_definitions() end,
+  { desc = 'Goto Definition' }
+)
+vim.keymap.set(
+  { 'n' },
+  'gD',
+  function() Snacks.picker.lsp_declarations() end,
+  { desc = 'Goto Declaration' }
+)
+-- vim.keymap.set({'n'}, "gr", function() Snacks.picker.lsp_references() end, nowait = true, {desc = "References" })
+vim.keymap.set(
+  { 'n' },
+  'gI',
+  function() Snacks.picker.lsp_implementations() end,
+  { desc = 'Goto Implementation' }
+)
+vim.keymap.set(
+  { 'n' },
+  'gy',
+  function() Snacks.picker.lsp_type_definitions() end,
+  { desc = 'Goto T[y]pe Definition' }
+)
+vim.keymap.set(
+  { 'n' },
+  'gai',
+  function() Snacks.picker.lsp_incoming_calls() end,
+  { desc = 'C[a]lls Incoming' }
+)
+vim.keymap.set(
+  { 'n' },
+  'gao',
+  function() Snacks.picker.lsp_outgoing_calls() end,
+  { desc = 'C[a]lls Outgoing' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>ss',
+  function() Snacks.picker.lsp_symbols() end,
+  { desc = 'LSP Symbols' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sS',
+  function() Snacks.picker.lsp_workspace_symbols() end,
+  { desc = 'LSP Workspace Symbols' }
+)
+-- require('todo-comments').setup{}
+vim.keymap.set(
+  { 'n' },
+  '<leader>st',
+  function() Snacks.picker.todo_comments() end,
+  { desc = 'Todo' }
+)
+vim.keymap.set(
+  { 'n' },
+  '<leader>sT',
+  function()
+    Snacks.picker.todo_comments { keywords = { 'TODO', 'FIX', 'FIXME' } }
+  end,
+  { desc = 'Todo/Fix/Fixme' }
 )
 vim.keymap.set(
   { 'n', 'v' },
-  '<leader>zz',
+  '<leader>sz',
   function() Snacks.zen() end,
-  { desc = 'Snacks.zen' }
+  { desc = 'Snacks zen' }
 )
 vim.keymap.set(
   { 'n', 'v' },
@@ -208,13 +558,13 @@ vim.keymap.set(
   { desc = 'message history(Snacks)' }
 )
 
-vim.cmd.packadd { 'nvim.undotree', bang = true }
-vim.keymap.set(
-  { 'n', 'v' },
-  '<leader>uu',
-  '<cmd>Undotree<CR>',
-  { desc = 'nvim.pack.undotree' }
-)
+-- vim.cmd.packadd { 'nvim.undotree', bang = true }
+-- vim.keymap.set(
+--   { 'n', 'v' },
+--   '<leader>uu',
+--   '<cmd>Undotree<CR>',
+--   { desc = 'nvim_pack_undotree' }
+-- )
 
 -- vim.pack.add { gh 'folke/zen-mode.nvim', gh 'folke/twilight.nvim' }
 -- require('zen-mode').setup {
