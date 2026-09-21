83b37caa2fcc7545312fbc768ef62363c9a7bc03
EssenMoon <yueqrgg@gmail.com>
2026-09-21 20:02:44 +0800

    del unused conf for X11

--- neovide/init.lua
+++
@@ -1,8 +0,0 @@
-if vim.g.neovide then
-  vim.o.guifont = 'Firacode Nerd Font:h15'
-  vim.o.linespace = 2
-
-  vim.g.neovide_opacity = 0.8
-  vim.g.neovide_normal_opacity = 0.8
-  vim.g.transparency = 0.8
-end
