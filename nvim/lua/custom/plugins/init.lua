5baf73aa3b4753969291fc48aaf5836b3d87cc7d
EssenMoon <yueqrgg@gmail.com>
2026-09-21 21:34:55 +0800

    use example dashboard

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -104,73 +104,74 @@ require('snacks').setup {
   bigfile = { enabled = true },
   dashboard = {
     enabled = true,
-    sections = {
-      { section = 'keys', gap = 1, padding = 1 },
-      {
-        pane = 2,
-        icon = ' ',
-        desc = 'Browse Repo',
-        padding = 1,
-        key = 'b',
-        action = function() Snacks.gitbrowse() end,
-      },
-      function()
-        local in_git = Snacks.git.get_root() ~= nil
-        local cmds = {
-          {
-            title = 'Notifications',
-            cmd = 'gh status',
-            action = function()
-              vim.ui.open 'https://github.com/notifications'
-            end,
-            key = 'n',
-            icon = ' ',
-            height = 5,
-            enabled = true,
-          },
-          {
-            title = 'Open Issues',
-            cmd = 'gh issue list -L 3',
-            key = 'i',
-            action = function()
-              vim.fn.jobstart('gh issue list --web', { detach = true })
-            end,
-            icon = ' ',
-            height = 3,
-          },
-          {
-            icon = ' ',
-            title = 'Open PRs',
-            cmd = 'gh pr list -L 3',
-            key = 'P',
-            action = function()
-              vim.fn.jobstart('gh pr list --web', { detach = true })
-            end,
-            height = 3,
-          },
-          {
-            icon = ' ',
-            title = 'Git Status',
-            cmd = 'git --no-pager diff --stat -B -M -C',
-            height = 3,
-          },
-        }
-        return vim.tbl_map(
-          function(cmd)
-            return vim.tbl_extend('force', {
-              pane = 2,
-              section = 'terminal',
-              enabled = in_git,
-              padding = 1,
-              ttl = 5 * 60,
-              indent = 3,
-            }, cmd)
-          end,
-          cmds
-        )
-      end,
-      { section = 'startup' },
-    },
+    example = 'compact_files',
+    -- sections = {
+    --   { section = 'keys', gap = 1, padding = 1 },
+    --   {
+    --     pane = 2,
+    --     icon = ' ',
+    --     desc = 'Browse Repo',
+    --     padding = 1,
+    --     key = 'b',
+    --     action = function() Snacks.gitbrowse() end,
+    --   },
+    --   function()
+    --     local in_git = Snacks.git.get_root() ~= nil
+    --     local cmds = {
+    --       {
+    --         title = 'Notifications',
+    --         cmd = 'gh status',
+    --         action = function()
+    --           vim.ui.open 'https://github.com/notifications'
+    --         end,
+    --         key = 'n',
+    --         icon = ' ',
+    --         height = 5,
+    --         enabled = true,
+    --       },
+    --       {
+    --         title = 'Open Issues',
+    --         cmd = 'gh issue list -L 3',
+    --         key = 'i',
+    --         action = function()
+    --           vim.fn.jobstart('gh issue list --web', { detach = true })
+    --         end,
+    --         icon = ' ',
+    --         height = 3,
+    --       },
+    --       {
+    --         icon = ' ',
+    --         title = 'Open PRs',
+    --         cmd = 'gh pr list -L 3',
+    --         key = 'P',
+    --         action = function()
+    --           vim.fn.jobstart('gh pr list --web', { detach = true })
+    --         end,
+    --         height = 3,
+    --       },
+    --       {
+    --         icon = ' ',
+    --         title = 'Git Status',
+    --         cmd = 'git --no-pager diff --stat -B -M -C',
+    --         height = 3,
+    --       },
+    -- }
+    --   return vim.tbl_map(
+    --     function(cmd)
+    --       return vim.tbl_extend('force', {
+    --         pane = 2,
+    --         section = 'terminal',
+    --         enabled = in_git,
+    --         padding = 1,
+    --         ttl = 5 * 60,
+    --         indent = 3,
+    --       }, cmd)
+    --     end,
+    --     cmds
+    --   )
+    -- end,
+    -- { section = 'startup' },
+    -- },
   },
   dim = { enabled = true },
   explorer = { enabled = true, replace_netrw = true, follow_file = true },
